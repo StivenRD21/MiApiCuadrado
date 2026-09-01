7edaa4c83282df9f5d2d3ec740f3e4364208d2fb
StivenRD21 <stivenramosd14@gmail.com>
2026-09-01 13:07:56 -0400

    Oculte mi contraseña de la base de datos por seguridad

--- obj/Release/net10.0/MiApiCuadrado.AssemblyInfo.cs
+++ obj/Release/net10.0/MiApiCuadrado.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiApiCuadrado")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b66470c1cbe94feaf8dad37b302338e4259f921")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiApiCuadrado")]
